b608722dc38c9efd0622edf6c1ee355d9bea513e
tanzizhen <923360732@qq.com>
2021-11-30 13:26:59 +0800

    修改试题样式

--- src/TopicArr/TopicCss.php
+++ src/TopicArr/TopicCss.php
@@ -12,7 +12,7 @@ namespace TopicHtml\TopicArr;
 class TopicCss
 {
     private $topic_font_size = '9pt';
-    private $topic_font_fam = 'STSongti';
+    private $topic_font_fam = 'SimSun';
 
     /**
      * @param string $topic_font_size
@@ -51,23 +51,27 @@ class TopicCss
         .topic-common {
             font-size: '.$this->topic_font_size.';
             font-family: \''.$this->topic_font_fam.'\';
+            page-break-inside: avoid !important;
+        }
+
+        .topic-common img {
+            vertical-align: middle;
         }
 
         .topic-stem' . TopicHtmlArr::$class_suffix . ' {
-            line-height: 16px;
+            line-height: 1.25em;
             text-align: justify;
-            vertical-align: middle;
-            margin-bottom: 8px;
+            margin-bottom: 1em;
         }
 
         .option-ul' . TopicHtmlArr::$class_suffix . ' {
             list-style: none;
-            margin-bottom: 8px;
+            margin-bottom: 1em;
         }
 
         .option-li' . TopicHtmlArr::$class_suffix . ' {
             position: relative;
-            line-height: 16px;
+            line-height: 1.25em;
         }
         
         .option-li' . TopicHtmlArr::$class_suffix . ' .option-label' . TopicHtmlArr::$class_suffix . ' {
@@ -77,14 +81,14 @@ class TopicCss
         }
         
         .option-li' . TopicHtmlArr::$class_suffix . ' .option-content' . TopicHtmlArr::$class_suffix . ' {
-            margin-left: 12px;
+            margin-left: 1em;
             text-align: justify;
         }
 
         .answer' . TopicHtmlArr::$class_suffix . ' {
             display: -webkit-flex;
             display: flex;
-            margin-bottom: 6px;
+            margin-bottom: 0.5em;
         }
         
         .answer' . TopicHtmlArr::$class_suffix . ' .long-empty {
@@ -93,34 +97,30 @@ class TopicCss
         }
 
         .answer' . TopicHtmlArr::$class_suffix . ' .answer-label' . TopicHtmlArr::$class_suffix . ' {
-            margin-left: -5px;
+            margin-left: -0.4em;
         }
 
-        .answer' . TopicHtmlArr::$class_suffix . ' .answer-content' . TopicHtmlArr::$class_suffix . ' img {
-            vertical-align: middle;
+        .analysis' . TopicHtmlArr::$class_suffix . ' {
+            position: relative;
         }
 
         .analysis' . TopicHtmlArr::$class_suffix . ' .analysis-label' . TopicHtmlArr::$class_suffix . ' {
-            margin-left: -5px;
-            margin-right: -5px;
+            position: absolute;
+            left: -0.4em;
         }
 
         .analysis' . TopicHtmlArr::$class_suffix . ' .analysis-content' . TopicHtmlArr::$class_suffix . ' {
             text-align: justify;
-            line-height: 16px;
-            display: inline
-        }
-
-        .analysis' . TopicHtmlArr::$class_suffix . ' .analysis-content' . TopicHtmlArr::$class_suffix . '  img {
-            vertical-align: middle;
+            text-indent: 3.5em;
+            line-height: 1.25em;
         }
         
         .y-4 li {
-            margin-bottom: 6px;
+            margin-bottom: 0.5em;
         }
 
         .y-4 li:last-child {
-            margin-bottom: 0px;
+            margin-bottom: 0;
         }
 
         .xy-2 {
@@ -134,16 +134,16 @@ class TopicCss
 
         .xy-2 li {
             width: 50%;
-            margin-bottom: 6px;
+            margin-bottom: 0.5em;
             box-sizing: border-box;
         }
 
         .xy-2 li:nth-child(odd) {
-            padding-right: 10px;
+            padding-right: 1em;
         }
 
         .xy-2 li:nth-child(n+3) {
-            margin-bottom: 0px;
+            margin-bottom: 0;
         }
 
         .x-4 {
@@ -158,7 +158,7 @@ class TopicCss
         }
         
         .x-4 li:nth-child(-n+3) {
-            padding-right: 10px;
+            padding-right: 1em;
         }
 
     </style>';
